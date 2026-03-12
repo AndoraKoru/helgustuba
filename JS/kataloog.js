160bf2ff9509e72be59856c24469c1279c126439
AndoraKoru <230125130+AndoraKoru@users.noreply.github.com>
2026-03-12 16:29:35 +0200

    12.03 changes

--- JS/kataloog.js
+++ JS/kataloog.js
@@ -1,54 +1,11 @@
 const products = [
     {
-        name: "JÄGALA",
-        img:      "Assets/Images/Catalogue/jägalaCataloguePicture.jpg",
-        imgSpecs: "Assets/Images/Catalogue/jägalaCataloguePicture2.jpg",
-        pdf: "#",
-        description: "Jägala juga on Eesti loodusjõu üks selgemaid sümboleid – lai, võimas ja iseloomult põhjamaine. Sama mõte kandub ka selle mudeli disaini: nurgeline vorm meenutab paekallast, tugev siluett loob ruumis kohalolu ning puhas joonekasutus mõjub sama selgelt nagu langev veekardin.",
-        specs: [
-            { label: "Mõõdud",           value: "660x415x465mm"                                              },
-            { label: "Kaal",             value: "46.5kg"                                                    },
-            { label: "Pinge/Sagedus",    value: "220V / 50Hz"                                               },
-            { label: "Garantii",         value: "2 + 5 aastat"                                              },
-            { label: "Loputusvee kogus", value: "2,7 l / 4,2 l"                                             },
-            { label: "Loputuskast",      value: "sisse ehitatud"                                            },
-            { label: "Väljavool",        value: "tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm)" },
-            { label: "Sertifikaat",      value: "CE"                                                        }
-        ]
-    },
-    {
-        name: "UKUORU",
-        img:      "Assets/Images/Catalogue/ukuoruCataloguePicture.jpg",
-        imgSpecs: "Assets/Images/Catalogue/ukuoruCataloguePicture2.jpg",
-        pdf: "#",
-        description: "Ukuoru on lihtsa ja voolujoonelise disainiga nutitualett, mille vorm mõjub pehmelt, puhtalt ja loomulikult. Inspireerituna Ukuoru joastiku sujuvast liikumisest, toob see mudel vannituppa tasakaalu, kergust ja vaikset elegantsi.",
-        specs: [
-            { label: "Mõõdud",           value: "660x415x465mm"                                              },
-            { label: "Kaal",             value: "46.5kg"                                                    },
-            { label: "Pinge/Sagedus",    value: "220V / 50Hz"                                               },
-            { label: "Garantii",         value: "2 + 5 aastat"                                              },
-            { label: "Loputusvee kogus", value: "2,7 l / 4,2 l"                                             },
-            { label: "Loputuskast",      value: "sisse ehitatud"                                            },
-            { label: "Väljavool",        value: "tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm)" },
-            { label: "Sertifikaat",      value: "CE"                                                        }
-        ]
-    },
-    {
         name: "KOHINA",
         img:      "Assets/Images/Catalogue/kohinaCataloguePicture.jpg",
         imgSpecs: "Assets/Images/Catalogue/kohinaCataloguePicture2.jpg",
         pdf: "#",
         description: "Kohina on vaikse kohalolu ilu. Nagu lühike juga, mis ei mõju oma suuruse, vaid oma iseloomu ja kerge heli kaudu, toob ka see mudel vannituppa õhulisust ja elegantsust.",
-        specs: [
-            { label: "Mõõdud",           value: "660x415x465mm"                                              },
-            { label: "Kaal",             value: "46.5kg"                                                    },
-            { label: "Pinge/Sagedus",    value: "220V / 50Hz"                                               },
-            { label: "Garantii",         value: "2 + 5 aastat"                                              },
-            { label: "Loputusvee kogus", value: "2,7 l / 4,2 l"                                             },
-            { label: "Loputuskast",      value: "sisse ehitatud"                                            },
-            { label: "Väljavool",        value: "tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm)" },
-            { label: "Sertifikaat",      value: "CE"                                                        }
-        ]
+        specs: "Mõõdud: 660x415x465mm. Kaal: 46.5kg. Pinge/Sagedus: 220V / 50Hz. Garantii: 2 + 5 aastat. Loputusvee kogus: 2,7 l / 4,2 l. Loputuskast: sisse ehitatud. Väljavool: tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm). Sertifikaat: CE."
     },
     {
         name: "VALASTE",
@@ -56,16 +13,23 @@ const products = [
         imgSpecs: "Assets/Images/Catalogue/valasteCataloguePicture2.jpg",
         pdf: "#",
         description: "Valaste on väljapaistva vormiga nutitualett, mille inspiratsioon pärineb Baltimaade kõrgeima tehisliku joa geoloogilisest ilust. Muudetavad värvikihtide lahendused ülaplaadil ja külgedel loovad disaini, mis meenutab paekalda kihistusi ja toob vannituppa sügavuse ning isikupära.",
-        specs: [
-            { label: "Mõõdud",           value: "660x415x465mm"                                              },
-            { label: "Kaal",             value: "46.5kg"                                                    },
-            { label: "Pinge/Sagedus",    value: "220V / 50Hz"                                               },
-            { label: "Garantii",         value: "2 + 5 aastat"                                              },
-            { label: "Loputusvee kogus", value: "2,7 l / 4,2 l"                                             },
-            { label: "Loputuskast",      value: "sisse ehitatud"                                            },
-            { label: "Väljavool",        value: "tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm)" },
-            { label: "Sertifikaat",      value: "CE"                                                        }
-        ]
+        specs: "Mõõdud: 660x415x465mm. Kaal: 46.5kg. Pinge/Sagedus: 220V / 50Hz. Garantii: 2 + 5 aastat. Loputusvee kogus: 2,7 l / 4,2 l. Loputuskast: sisse ehitatud. Väljavool: tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm). Sertifikaat: CE."
+    },
+    {
+        name: "UKUORU",
+        img:      "Assets/Images/Catalogue/ukuoruCataloguePicture.jpg",
+        imgSpecs: "Assets/Images/Catalogue/ukuoruCataloguePicture2.jpg",
+        pdf: "#",
+        description: "Ukuoru on lihtsa ja voolujoonelise disainiga nutitualett, mille vorm mõjub pehmelt, puhtalt ja loomulikult. Inspireerituna Ukuoru joastiku sujuvast liikumisest, toob see mudel vannituppa tasakaalu, kergust ja vaikset elegantsi.",
+        specs: "Mõõdud: 660x415x465mm. Kaal: 46.5kg. Pinge/Sagedus: 220V / 50Hz. Garantii: 2 + 5 aastat. Loputusvee kogus: 2,7 l / 4,2 l. Loputuskast: sisse ehitatud. Väljavool: tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm). Sertifikaat: CE."
+    },
+    {
+        name: "JÄGALA",
+        img:      "Assets/Images/Catalogue/jägalaCataloguePicture.jpg",
+        imgSpecs: "Assets/Images/Catalogue/jägalaCataloguePicture2.jpg",
+        pdf: "#",
+        description: "Jägala juga on Eesti loodusjõu üks selgemaid sümboleid – lai, võimas ja iseloomult põhjamaine. Sama mõte kandub ka selle mudeli disaini: nurgeline vorm meenutab paekallast, tugev siluett loob ruumis kohalolu ning puhas joonekasutus mõjub sama selgelt nagu langev veekardin.",
+        specs: "Mõõdud: 660x415x465mm. Kaal: 46.5kg. Pinge/Sagedus: 220V / 50Hz. Garantii: 2 + 5 aastat. Loputusvee kogus: 2,7 l / 4,2 l. Loputuskast: sisse ehitatud. Väljavool: tahavooluga 180 mm (Allavooluga variant on tellitav 300-400 mm). Sertifikaat: CE."
     }
 ];
 
@@ -97,9 +61,7 @@ document.querySelectorAll('.kataloogStage').forEach((stage, i) => {
             specs.style.display     = 'none';
         } else {
             subtitle.textContent    = 'Tehnilised andmed';
-            specs.innerHTML         = product.specs
-                .map(s => `<div class="specRow"><dt>${s.label}</dt><dd>${s.value}</dd></div>`)
-                .join('');
+            specs.innerHTML         = `<p>${product.specs}</p>`;
             features.style.display  = 'none';
             specs.style.display     = '';
         }
